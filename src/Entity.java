0c06caa753e0a6593c6b492b29507b3c0ba6ca8d
magskwa <magali.egger@heig-vd.ch>
2022-02-23 11:34:49 +0100

    entity

--- src/Entity.java
+++ src/Entity.java
@@ -1,3 +1,35 @@
-public class Entity {
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+
+abstract public class Entity {
+    private Vector position;
+    private Vector deplacement;
+    private Color color;
+    private int taille;
+    private Random rand;
+    private Frame f;
+
+    public Entity(){
+        color = new Color(rand.nextInt());
+        taille = rand.nextInt();
+        position = new Vector(rand.nextInt(), rand.nextInt());
+        deplacement = new Vector(rand.nextInt(), rand.nextInt());
+    }
+
+    Vector getPosition() {
+        return position;
+    }
+
+    Vector getDeplacement() {
+        return deplacement;
+    }
+
+    int getTaille() {
+        return taille;
+    }
+
+    abstract public void draw(Graphics graphics);
 
 }
