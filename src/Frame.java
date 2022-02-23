5bc8173fd0765faf1ba60485c038d37ef53d68cb
NunezTania <tnunez@bluewin.ch>
2022-02-23 11:49:48 +0100

    Frame class

--- src/Frame.java
+++ src/Frame.java
@@ -1,13 +1,26 @@
 
 import javax.swing.*;
 
-public class Frame {
+public class Frame extends JFrame {
 
+    Frame() { super(); }
+
+    public void run(int nSquares, int nCircles) {
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        Cercle c = new Cercle(new Vector(0, 0), new Vector(0, 0), 50);
+        this.add(c);
+        c.draw();
+
+
+
+    }
 
     public static void main(String[] args){
         Frame f = new Frame();
-        JFrame frame = new JFrame();
-        frame.setVisible(true);
+        f.run(0, 0);
     }
 }
 
