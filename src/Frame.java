074f4004520609d343e1e06cd24dcc4c159d8f72
magskwa <magali.egger@heig-vd.ch>
2022-02-23 10:48:56 +0100

    begin

---
+++ src/Frame.java
@@ -0,0 +1,13 @@
+
+import javax.swing.*;
+
+public class Frame {
+
+
+    public static void main(String[] args){
+        Frame f = new Frame();
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+    }
+}
+
